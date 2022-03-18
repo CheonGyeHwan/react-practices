b1754a8a844d9870b22d19886ecca69bb020cab9
CheonGyeHwan <cgh5531@naver.com>
2022-03-18 14:42:02 +0900

    emaillist #finish

--- emaillist/src/App.js
+++ emaillist/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import './assets/scss/App.scss';
+import Emaillist from './Emaillist';
+import RegisterForm from './RegisterForm';
+import Searchbar from './Searchbar';
+import data from './assets/json/data.json';
 
 const App = () => {
   return (
-    <div>Emaillist</div>
-  )
+    <div className={ 'App' }>
+        <RegisterForm />
+        <Searchbar />
+        <Emaillist emails={ data }/>
+    </div>
+  );
 }
 
 export default App;
