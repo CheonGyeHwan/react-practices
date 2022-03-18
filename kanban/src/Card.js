f7c5ae62a550eb7db7a3fb0de30a8fa431ce7d7b
CheonGyeHwan <cgh5531@naver.com>
2022-03-18 17:20:35 +0900

    state # task, card

--- kanban/src/Card.js
+++ kanban/src/Card.js
@@ -5,14 +5,21 @@ import TaskList from './TaskList';
 const Card = ({ card }) => {
     const [showDetails, setShowDetails] = useState(false);
 
+    const clickTitle = function(e) {
+        e.target.className = (e.target.className === styles.Card__Title ? styles.Card__Title__open : styles.Card__Title);
+        setShowDetails(!showDetails);
+    }
+
     return (
         <>
             <div className={ styles.Card }>
-            <div className={ styles.Card__Title }>{ card.title }</div>
-                <div className={ styles.Card__Details }>
-                    { card.description }
-                </div>
-            <TaskList taskList={ card.tasks } />
+            <div className={ styles.Card__Title } onClick={ clickTitle }>{ card.title }</div>
+            <div className={ styles.Card__Details }>
+                { card.description }
+            </div>
+            {
+                showDetails ? <TaskList taskList={ card.tasks } /> : null
+            }
             </div>
         </>
     );
--- kanban/src/TaskList__Task.js
+++ kanban/src/TaskList__Task.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState ,useCallback} from 'react';
 import styles from './assets/css/TaskList__Task.css';
 
 const TaskList__Task = ({ task }) => {
-  return (
-      <>
-          <li className={ styles.TaskList__Task }>
-              <input type={ 'checkbox' } defaultChecked={ task.done } />
-              { task.name }
-              <a href={ '#' } className={ styles.TaskList__Task__remove }></a>
-          </li>
-      </>
+    const [taskCheck, setTaskCheck] = useState(task.done);
+
+    const onChangetaskCheck = (e) =>{
+        task.done = !taskCheck;
+        setTaskCheck(!taskCheck);
+    }
+
+    return (
+        <>
+            <li className={ styles.TaskList__Task }>
+                <input type={ 'checkbox' } value= { taskCheck } checked={ taskCheck } onChange={ onChangetaskCheck } />
+                { task.name }
+                <a href={ '#' } className={ styles.TaskList__Task__remove }></a>
+            </li>
+        </>
   );
 }
 
