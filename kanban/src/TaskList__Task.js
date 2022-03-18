5025debf59f1730f2ef8cba0e1d826f222521dc6
CheonGyeHwan <cgh5531@naver.com>
2022-03-18 17:45:34 +0900

    state # task, card

--- kanban/src/TaskList__Task.js
+++ kanban/src/TaskList__Task.js
@@ -1,4 +1,4 @@
-import React, { useState ,useCallback} from 'react';
+import React, { useState } from 'react';
 import styles from './assets/css/TaskList__Task.css';
 
 const TaskList__Task = ({ task }) => {
