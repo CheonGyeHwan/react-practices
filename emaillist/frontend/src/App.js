39274b5eeea520ee0023cd47e5fb1cd9e0bd62bb
CheonGyeHwan <cgh5531@naver.com>
2022-03-24 12:08:32 +0900

    emaillist #3

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './assets/scss/App.scss';
 import Emaillist from './Emaillist';
 import RegisterForm from './RegisterForm';
@@ -8,6 +8,19 @@ import data from './assets/json/data.json';
 const App = () => {
   const [emails, setEmails] = useState(data);
   // const [keyword, setKeyword] = useState("");
+
+  useEffect(async () => {
+    const response = await fetch('/api', {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: null
+    });
+
+    console.log(response);
+  }, []);
 
   const NotifyKeywordChange = function(kw) {
     // setKeyword(kw);
